e8cefb414c3095e22ee062e23c9cb13640e80d20
Ger Hobbelt <ger@hobbelt.com>
2017-10-28 16:17:10 +0200

    installed NPM package-lock.json, added gitattr file to ensure proper CR/CRLF handling on ALL dev platforms; regenerated library files.

--- dist/js-yaml.js
+++ dist/js-yaml.js
@@ -1,4 +1,14 @@
-/* js-yaml 3.10.0 https://github.com/nodeca/js-yaml */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.jsyaml = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/* @gerhobbelt/js-yaml 3.10.0 https://github.com/nodeca/@gerhobbelt/js-yaml */
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.jsyaml = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"/":[function(require,module,exports){
+'use strict';
+
+
+var yaml = require('./lib/js-yaml.js');
+
+
+module.exports = yaml;
+
+},{"./lib/js-yaml.js":1}],1:[function(require,module,exports){
 'use strict';
 
 
@@ -218,8 +228,7 @@ function State(options) {
   this.noRefs       = options['noRefs'] || false;
   this.noCompatMode = options['noCompatMode'] || false;
   this.condenseFlow = options['condenseFlow'] || false;
-  // accept a `quoteKeys` option *explicitly* set to FALSE:
-  this.quoteKeys    = (options['quoteKeys'] != null ? options['quoteKeys'] : !!this.condenseFlow);
+  this.quoteKeys = options['quoteKeys'] || false;
   if (this.quoteKeys) {
     if (this.quoteKeys === true) {
       this.quoteKeys = '"';
@@ -577,7 +586,7 @@ function escapeString(string) {
   var escapeSeq;
 
   for (var i = 0; i < string.length; i++) {
-    char = string.charCodeAt(i);
+    char = string.charCodeAt(i);              // Only very recent JS engines support .codePointAt(i) API
     // Check for surrogate pairs (reference Unicode 3.0 section "3.7 Surrogates").
     if (char >= 0xD800 && char <= 0xDBFF/* high surrogate */) {
       nextChar = string.charCodeAt(i + 1);
@@ -669,7 +678,7 @@ function writeFlowMapping(state, level, object) {
     pairBuffer += state.dump;
     pairBuffer += state.quoteKeys || '';
     pairBuffer += ':';
-    if (!state.condenseFlow) {
+    if (!(state.condenseFlow && state.quoteKeys)) {
       pairBuffer += ' ';
     }
 
@@ -3441,7 +3450,7 @@ try {
   esprima = _require('esprima');
 } catch (_) {
   /*global window */
-  if (typeof window !== 'undefined') esprima = window.esprima;
+  if (typeof window !== 'undefined') { esprima = window.esprima; }
 }
 
 var Type = require('../../type');
@@ -3903,14 +3912,5 @@ module.exports = new Type('tag:yaml.org,2002:timestamp', {
   represent: representYamlTimestamp
 });
 
-},{"../type":13}],"/":[function(require,module,exports){
-'use strict';
-
-
-var yaml = require('./lib/js-yaml.js');
-
-
-module.exports = yaml;
-
-},{"./lib/js-yaml.js":1}]},{},[])("/")
+},{"../type":13}]},{},[])("/")
 });
