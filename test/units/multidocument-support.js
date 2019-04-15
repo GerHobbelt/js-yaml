95570f63525e3f8248bff4695b719296e71af842
Ger Hobbelt <ger@hobbelt.com>
2019-04-15 22:33:47 +0200

    added test to showcase the document listener callback consuming arbitrary content between yaml documents. Augmented the listener API: when it returns a truthy value, this tells js-yaml that another YAML block/document is expected (or EOF is reached); this allows for easy consuming of arbitrary content, e.g. multidocument markdown+YAML inputs.

--- test/units/multidocument-support.js
+++ test/units/multidocument-support.js
@@ -96,4 +96,79 @@ suite('Multidocument source using `loadAll`', function () {
     assert.strictEqual(docs.length, 4);
     assert.deepEqual(docs, expected);
   });
+
+  test('should support cursor forwarding in the document listener', function () {
+    var count = 0;
+    var posCheck = [ [ 0, 9 ], [ 46, 55 ] ];
+
+    function docListener(doc, index, state) {
+      assert.strictEqual(index, count);
+      count++;
+
+      assert.strictEqual(state.documentStart, posCheck[index][0]);
+      assert.strictEqual(state.position, posCheck[index][1]);
+
+      var pos1 = state.input.indexOf('...\n', state.position);
+      assert.strictEqual(pos1, state.position);
+      var pos2 = state.input.indexOf('\n...\n', pos1);
+      if (pos2 < 0) {
+        // EOF reached in content chunk:
+        pos2 = state.length;
+      } else {
+        pos2++;     // skip first LF
+      }
+
+      // position the cursor at the start of the marker (iff it's a `...` or `---` marker),
+      // at least before the next LF:
+      state.position = pos2;
+      // extra: also patch the `lineStart` value:
+      state.lineStart = pos2;
+
+      // update the line count for correct line tracking in diagnostic reports:
+      var content = state.input.slice(pos1, pos2);
+      var lines = content.split('\n');
+      state.line += lines.length - 1;
+
+      // augment the document YAML (& strip off lead and tail markers):
+      // (js-yaml has added an extra LF at the end of your content, so we always
+      // step back 1 from the end, hence the unconditional `-1` for the `.slice()`)
+      content = content.slice(4, -1);
+      doc.__content__ = content;
+
+      // signal to the js-yaml library that we expect another YAML chunk coming up next!
+      return true;
+    }
+
+    var docs = yaml.loadAll('foo: bar\n...\nbar: bla bla\n\nx: y\n\nz: bogus\n...\nfoo: bar\n...\nfoo\n\nfoo',
+      docListener, {
+        // also store position information: this test does double duty that way...
+        metaKey: 'coord'
+      });
+
+    var expected = [
+      {
+        coord: {
+          foo: {
+            startLine: 0,
+            startPos: 0
+          }
+        },
+        foo: 'bar',
+        __content__: 'bar: bla bla\n\nx: y\n\nz: bogus'
+      },
+      {
+        coord: {
+          foo: {
+            startLine: 8,
+            startPos: 46
+          }
+        },
+        foo: 'bar',
+        __content__: 'foo\n\nfoo'
+      }
+    ];
+    assert.strictEqual(count, 2);
+    assert.strictEqual(docs.length, 2);
+    assert.deepEqual(docs, expected);
+  });
 });
